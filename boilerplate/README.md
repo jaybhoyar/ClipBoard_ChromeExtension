7ba0b020579890b3be91e314ec948148e76509ea
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-06-23 13:47:08 +0530

    fix: files,layout

---
+++ boilerplate/README.md
@@ -0,0 +1,37 @@
+# Variables & Types
+
+The aim of the exercise we will be building a Chrome Extension using concepts like
+* [ ] DOM
+* [ ] Events
+* [ ] Array Methods
+* [ ] LocalStoreage
+
+## Getting Started
+
+**Setup your project by following these instructions:**
+
+* [ ] Make your own copy by forking this exercise.
+* [ ] Go to the settings of the newly forked repo and add your mentor as a collaborator on the repo.
+* [ ] Make a clone of your forked repo.
+* [ ] Create a new branch called develop: `git checkout -b develop`.
+* [ ] Work on the exercise on your `develop` branch.
+* [ ] Push commits to `develop` branch ONLY. Command - `git push origin develop`.
+
+
+**After you are done.**
+
+* [ ] Submit a Pull Request to merge `develop` branch into the `master` branch of your repo. **Please don't merge your own pull request.**
+* [ ] Add your mentor as a reviewer on the PR.
+* [ ] The assigned mentor will review the exercise and pass feedback.
+* [ ] Once the mentor merges the PR to `master` branch, you are done with this exercise. Yay!
+
+## Goals 🎯
+* [ ] Use the boilerplate folder for the basic setup of the chrome extension.
+* [ ] List your functionalities and create them.
+
+## Steps To Run Chrome Extension 🛹
+* [ ] Open `Chrome` click on the menu on the right `setting` ➡️ `More Tools`  ➡️ `Extensions`
+* [ ] Switch On the `developer mode` on the right top corner.
+* [ ] Click on `load unpacked` on the left and select the `boilerplate` folder.
+* [ ] Now if you open a new tab you can see `Hello World!`
+* [ ] Cheers 💐 you are done. Start working with the respective files.
--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,51 +1,41 @@
 // You js goes here
 let textArea = document.querySelector(".text_input");
 let search = document.querySelector(".search");
-let texttitle = document.querySelector(".text_input_title");
+
 let container = document.querySelector(".container_2");
 let clips = JSON.parse(localStorage.getItem("clipArr")) || [];
 let id = Date.now();
-let gSearch = document.getElementById("the_search_box");
-let textToCopy = document.querySelector(".content_styles");
+
 function createClips(event) {
-	if (
-		event.keyCode == 13 &&
-		event.target.value.trim() != "" &&
-		texttitle.value.trim() != ""
-	) {
-		console.log(event.target.value, texttitle.value);
+	if (event.keyCode == 13 && event.target.value.trim() != "") {
+		console.log(event.target.value);
 		const clip = {
-			title: texttitle.value,
 			content: event.target.value,
-			id: ++id
+			id: ++id,
 		};
 		clips.push(clip);
 		localStorage.setItem("clipArr", JSON.stringify(clips));
 		event.target.value = "";
-		texttitle.value = "";
 		clips = JSON.parse(localStorage.getItem("clipArr"));
 		viewClips(clips);
 	}
 }
 function viewClips(clipsArr) {
 	container.innerHTML = "";
-	clipsArr.forEach(clip => {
+	clipsArr.forEach((clip) => {
 		let parentDiv = document.createElement("div");
-		let titleDiv = document.createElement("div");
+
 		let contentDiv = document.createElement("div");
 		let titleP = document.createElement("p");
 		let spanX = document.createElement("span");
 		spanX.classList.add("spanX");
 		spanX.textContent = "X";
-		titleDiv.classList.add("title_styles");
 		contentDiv.classList.add("content_styles");
 		parentDiv.setAttribute("data-id", clip.id);
-		parentDiv.classList.add("show_clip", "show_clip_header");
-		parentDiv.draggable = "true";
+		parentDiv.classList.add("show_clip");
 		titleP.innerText = clip.title;
-		contentDiv.innerHTML = clip.content;
-		titleDiv.append(titleP, spanX);
-		parentDiv.append(titleDiv, contentDiv);
+		contentDiv.innerText = clip.content;
+		parentDiv.append(contentDiv);
 		container.append(parentDiv);
 		spanX.addEventListener("click", deleteClip);
 		contentDiv.addEventListener("click", selectValue);
@@ -53,18 +43,20 @@ function viewClips(clipsArr) {
 }
 function deleteClip(event) {
 	let target = event.target.parentElement;
-	// console.log(target.parentElement);
-	clips = clips.filter(clip => !(target.parentElement.dataset.id == clip.id));
+
+	clips = clips.filter(
+		(clip) => !(target.parentElement.dataset.id == clip.id)
+	);
 	localStorage.setItem("clipArr", JSON.stringify(clips));
 	viewClips(clips);
 }
 function selectValue(event) {
-  var range = document.createRange();
-  textToCopy
-	range.selectNode(textToCopy);
+	var range = document.createRange();
+	range.selectNode(event.target);
 	window.getSelection().removeAllRanges(); // clear current selection
 	window.getSelection().addRange(range); // to select text
 	document.execCommand("copy");
+	//window.getSelection().removeAllRanges(); // to deselect
 	setTimeout(addCopied, 0);
 	setTimeout(removeCopied, 2000);
 	function addCopied() {
@@ -75,49 +67,19 @@ function selectValue(event) {
 		event.target.style.fontSize = "1.5rem";
 	}
 	function removeCopied() {
-		// // event.target.innerText = text;
 		viewClips(clips);
-		// event.target.style.fontWeight = "400";
-		// // event.target.style.fontSize = "1rem";
-		// // event.target.style.color = "#000";
 	}
 }
-
 function searchClips(event) {
-	if (event.target.value != "") {
-		let searchWord = event.target.value.toLowerCase();
-		debugger;
-		let searchedArr = clips.filter(clip =>
-			clip.title.toLowerCase().includes(searchWord)
-		);
-		viewClips(searchedArr);
+	if (event.keyCode === 13 && event.target.value.trim() != "") {
+		let searchTitle = event.target.value;
+		let searched = clips.filter((clip) => clip.title == searchTitle);
+		viewClips(searched);
 	} else {
 		viewClips(clips);
 	}
 }
-function GoogleSearch(event) {
-	if (event.keyCode === 13) {
-		var box = document.getElementById("the_search_box");
-		window.location.href =
-			"http://www.google.com/search?q=" + escape(box.value);
-	}
-}
-// function randomColor() {
-// 	let words = "ABCDEF0123456789".split("");
-// 	let color = "#";
-// 	for (i = 0; i < 6; i++) {
-// 		color += words[Math.floor(Math.random() * 16)];
-// 	}
-// 	return color;
-// }
+
 viewClips(clips);
 
-// document.querySelector("body").style.backgroundColor = randomColor();
-// viewClips(clips);
-// textArea.addEventListener("keydown", createClips);
 textArea.addEventListener("keydown", createClips);
-search.addEventListener("keydown", searchClips);
-gSearch.addEventListener("keydown", GoogleSearch);
-
-//
-//  copy
